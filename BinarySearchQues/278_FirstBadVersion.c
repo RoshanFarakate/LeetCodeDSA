a310c5a48eb4c5e6d76d4ce69d2af582392dcdcd
Roshan Farakate <88421025+RoshanFarakate@users.noreply.github.com>
2022-01-10 18:42:15 +0530

    Create 278_FirstBadVersion.c

---
+++ BinarySearchQues/278_FirstBadVersion.c
@@ -0,0 +1,20 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+int firstBadVersion(int n) {
+    int i,start=0,end=n,middle,ans;
+    middle=(start+end)/2;
+    while(start<=end)
+    {
+        middle = start + (end - start)/2;
+        if(isBadVersion(middle)==true)
+        {
+            ans = middle;
+            end = middle-1;
+        }
+        else{
+            start = middle+1;
+        }
+    }
+    return ans;
+}
