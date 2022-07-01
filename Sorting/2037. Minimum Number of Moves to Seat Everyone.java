f35e372762ee77ece8a0990b762aa0335c34da5c
Roshan Farakate <88421025+RoshanFarakate@users.noreply.github.com>
2022-07-01 23:43:56 +0530

    Create 2037. Minimum Number of Moves to Seat Everyone.java

---
+++ Sorting/2037. Minimum Number of Moves to Seat Everyone.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minMovesToSeat(int[] seats, int[] students) {
+        Arrays.sort(seats);
+        Arrays.sort(students);
+        int dif=0;
+        for(int i=0;i<seats.length;i++){
+            dif=dif+Math.abs(students[i]-seats[i]);
+        }
+        return dif;
+    }
+}
