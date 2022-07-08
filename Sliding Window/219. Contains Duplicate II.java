a053ecd35167840249c11950263d3251948c197e
Roshan Farakate <88421025+RoshanFarakate@users.noreply.github.com>
2022-07-08 22:04:22 +0530

    Create 219. Contains Duplicate II.java

---
+++ Sliding Window/219. Contains Duplicate II.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer,Integer> h=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+        if(h.containsKey(nums[i]) && i-h.get(nums[i])<=k) return true;
+        else{
+            h.put(nums[i],i);
+        }
+    }
+    return false;
+    }
+}
